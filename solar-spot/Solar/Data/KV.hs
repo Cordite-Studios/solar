0fa2f6241eb6752935d3eecf7bb539525c9c3326
Levi Schuck <levi.schuck@gmail.com>
2013-09-13 13:50:18 -0600

    Type errors much better now, also documentation added

--- solar-spot/Solar/Data/KV.hs
+++ solar-spot/Solar/Data/KV.hs
@@ -1,7 +1,6 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Solar.Data.KV
-    ( KVClass
-    , KVTime(..)
+    ( KVTime(..)
     , KVLink(..)
     , KVMeta(..)
     , KV(..)
@@ -17,37 +16,52 @@ import Data.Typeable
 import Data.Time.Format (formatTime, readsTime, ParseTime(..))
 import System.Locale (defaultTimeLocale)
 
-type KVClass c = c
 type KVTime    = UTCTime 
 
 data KVIdentifier n = KVIdentifier
     { namespace :: !n
+    -- ^ Namespace enumeration for where this belongs
     , key       :: !Text
+    -- ^ Textual name of the key that can be looked up
     } deriving (Show, Read)
 
 data KVLink n r c = KVLink
     { linkIdentifier :: !(KVIdentifier n)
+    -- ^ Identity of the remote entity
     , linkRelations  :: ![r]
-    , linkClasses    :: ![KVClass c]
+    -- ^ Relation Enums that describe what it means
+    , linkClasses    :: ![c]
+    -- ^ Class Enums that describe what the entity is
     } deriving (Show, Read)
 
 data KVMeta namespace relations classes = KVMeta
     { identifier   :: !(KVIdentifier namespace)
     -- ^ Identifier for this entity
-    , classes      :: ![KVClass classes]
-    -- ^ The classifications 
+    , classes      :: ![classes]
+    -- ^ The classifications for this entity
     , relations    :: ![KVLink namespace relations classes]
+    -- ^ Links that this entity has with others
     , lastModified :: !KVTime
+    -- ^ Modification date for the entire data element
     , invalid      :: !Bool
+    -- ^ Invalidation flag
     } deriving (Show, Read)
 
 data KV namespace relations classes datas cache = KV
-    { content       :: !(datas namespace relations classes)
-    , meta          :: !(KVMeta namespace relations classes)
+    { meta          :: !(KVMeta namespace relations classes)
+    -- ^ Meta data for this entity
+    , content       :: !(datas namespace relations classes)
+    -- ^ Content for this entity
     , caches        :: !(Maybe (cache namespace relations classes))
+    -- ^ Caches, optional, use 'kvNoCache' function or 'KVNoCache'
+    -- data type if you don't plan for this entity to ever have
+    -- caches.
     } deriving (Show, Read)
 
-data KVNoCache n r c = KVNoCache deriving (Show, Read, Typeable, Eq, Ord)
+-- | The "No Cache" data type, use 'kvNoCache' to provide a typed 'Nothing'
+data KVNoCache n r c = KVNoCache
+    deriving (Show, Read, Typeable, Eq, Ord)
 
+-- | Gives a typed 'Nothing' of 'KVNoCache'
 kvNoCache :: (Maybe (KVNoCache n r c))
 kvNoCache = Nothing
