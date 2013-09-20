076c052b81d87a8a5d7a9649b58d68ad35008e96
Levi Schuck <levi.schuck@gmail.com>
2013-09-20 16:18:46 -0600

    Adding documentation, increasing Context usage.

--- solar-cells/Solar/Storage.hs
+++ solar-cells/Solar/Storage.hs
@@ -1,58 +1,98 @@
 {-# LANGUAGE Rank2Types #-}
-module Solar.Storage where
+module Solar.Storage
+    (
+    -- * Types
+      Context
+    , Storage(..)
+    -- * Manipulators
+    , get
+    , put
+    , del
+    , Solar.Storage.invalidate
+    -- * Helpers
+    , noContext
+    )
+where
 
 import           Solar.Data.KV as K
 import           Data.Default.Class
 import qualified Data.Foldable as F
 import qualified Control.Monad as M
+import qualified Data.Typeable as T
+import qualified Data.Dynamic  as D
+import qualified Data.Map      as Map
 
-data KVStorage r n c d c' = KVStorage
-    { getPersistant :: (Monad m) => KVIdentifier n -> m (Maybe (KV r n c d c'))
-    , getCached     :: (Monad m) => KVIdentifier n -> m (Maybe (KV r n c d c'))
-    , putPersistant :: (Monad m) => KV r n c d c' -> m (KV r n c d c')
-    , putCached     :: (Monad m) => KV r n c d c' -> m ()
-    , delPersistant :: (Monad m) => KVIdentifier n -> m Bool
-    , delCached     :: (Monad m) => KVIdentifier n -> m Bool
+-- | Contexts are needed for instances like riak
+-- which comes with a vector clock.
+-- 
+-- It may also be used to store connections or
+-- pools.
+type Context = Map.Map T.TypeRep D.Dynamic
+
+data Storage r n c d c' = Storage
+    { getPersistant :: (Monad m) => Context -> KVIdentifier n -> m (Maybe (KV r n c d c', Context))
+    -- ^ Given an identifier, provide a result from the
+    -- persistant medium
+    , getCached     :: (Monad m) => Context -> KVIdentifier n -> m (Maybe (KV r n c d c', Context))
+    -- ^ Given an identifier, lookup in the cache the value
+    , putPersistant :: (Monad m) => Context -> KV r n c d c' ->  m (KV r n c d c', Context)
+    -- ^ Put this content into the persistant medium
+    , putCached     :: (Monad m) => Context -> KV r n c d c' -> m ()
+    -- ^ Put this content into the cache medium
+    , delPersistant :: (Monad m) => Context -> KVIdentifier n -> m Bool
+    -- ^ Permanently remove (not 'invalidate') the entity
+    -- from the persistant medium
+    , delCached     :: (Monad m) => Context -> KVIdentifier n -> m Bool
+    -- ^ Clear this entry from the cache (not 'invalidate')
     }
 
-instance Default (KVStorage r n c d c') where
-    def = KVStorage
-        { getPersistant = \_ -> return Nothing
-        , getCached = \_ -> return Nothing
-        , putPersistant = return
-        , putCached = \_ -> return ()
-        , delPersistant = \_ -> return False
-        , delCached = \_ -> return False
+instance Default (Storage r n c d c') where
+    def = Storage
+        { getPersistant = \_ _ -> return Nothing
+        , getCached = \_ _ -> return Nothing
+        , putPersistant = \a b -> return (b, a)
+        , putCached = \_ _ -> return ()
+        , delPersistant = \_ _ -> return False
+        , delCached = \_ _ -> return False
         }
 
-put :: (Monad m) => KVStorage r n c d c' -> KV r n c d c' -> m ()
-put store kv = do
-    kv' <- putPersistant store kv
+put :: (Monad m) => Context -> Storage r n c d c' -> KV r n c d c' -> m ()
+put c' store kv = do
+    M.when (Map.size c' == 0) $ fail failMessage
+    (kv', c) <- putPersistant store c' kv
     -- ↑ other storages may modify the kv, such as
     -- riak, since it may resolve merge conflicts. 
-    putCached store kv'
+    putCached store (Map.union c c') kv'
 {-# INLINABLE put #-}
 
-get :: (Monad m) => KVStorage r n c d c' -> KVIdentifier n -> m (Maybe (KV r n c d c'))
-get store i = do
-    ckv <- getCached store i
+get :: (Monad m) => Context -> Storage r n c d c' -> KVIdentifier n -> m (Maybe (KV r n c d c', Context))
+get c' store i = do
+    M.when (Map.size c' == 0) $ fail failMessage
+    ckv <- getCached store c' i
     case ckv of
-        Just (kv) -> return ckv
+        Just (kv, _) -> return ckv
         Nothing -> do
-            pkv <- getPersistant store i
-            F.forM_ pkv (\v -> putCached store v)
+            pkv <- getPersistant store c' i
+            F.forM_ pkv (\(v, c) -> putCached store (Map.union c c') v)
             return pkv
 {-# INLINABLE get #-}
 
-del ::  (Monad m) => KVStorage r n c d c' -> KVIdentifier n -> m Bool
-del store i =
-    M.liftM or $ sequence [c, p]
+del ::  (Monad m) => Context -> Storage r n c d c' -> KVIdentifier n -> m Bool
+del c store i =
+    M.liftM or $ sequence [c', p]
     where
-        p = delPersistant store i
-        c = delCached store i
+        p = delPersistant store c i
+        c' = delCached store c i
 {-# INLINABLE del #-}
 
-invalidate :: (Monad m) => KVStorage r n c d c' -> KVIdentifier n -> m ()
-invalidate store i =
-    get store i >>= F.mapM_ (\v -> put store (K.invalidate v))
+invalidate :: (Monad m) => Context -> Storage r n c d c' -> KVIdentifier n -> m ()
+invalidate c' store i =
+    get c' store i >>= F.mapM_ (\(v, c) -> put (Map.union c c') store (K.invalidate v))
 {-# INLINABLE invalidate #-}
+
+noContext :: Context
+noContext = Map.empty
+{-# INLINABLE noContext #-}
+
+failMessage = "Empty Context Provided, no possible outcome!"
+{-# INLINABLE failMessage #-}
