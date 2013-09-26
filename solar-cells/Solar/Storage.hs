f04d3b62e49c6740c795f9b74db077e99f96320b
Levi Schuck <levi.schuck@gmail.com>
2013-09-26 16:24:48 -0600

    Refactored into StateT

--- solar-cells/Solar/Storage.hs
+++ solar-cells/Solar/Storage.hs
@@ -5,9 +5,9 @@ module Solar.Storage
       Context
     , Storage(..)
     -- * Manipulators
-    , get
-    , put
-    , del
+    , getS
+    , putS
+    , delS
     , Solar.Storage.invalidate
     -- * Helpers
     , noContext
@@ -24,6 +24,7 @@ import qualified Control.Monad as M
 import qualified Data.Typeable as T
 import qualified Data.Dynamic  as D
 import qualified Data.Map      as Map
+import Control.Monad.Trans.State as ST
 
 -- | Contexts are needed for instances like riak
 -- which comes with a vector clock.
@@ -33,82 +34,86 @@ import qualified Data.Map      as Map
 type Context = Map.Map T.TypeRep D.Dynamic
 
 data Storage n r c d c' = Storage
-    { getPersistant :: (Monad m) => Context -> KVIdentifier n -> m (Maybe (KV n r c d c', Context))
+    { getPersistant :: (Monad m) => KVIdentifier n -> StateT Context m (Maybe (KV n r c d c'))
     -- ^ Given an identifier, provide a result from the
     -- persistant medium
-    , getCached     :: (Monad m) => Context -> KVIdentifier n -> m (Maybe (KV n r c d c', Context))
+    , getCached     :: (Monad m) => KVIdentifier n -> StateT Context m (Maybe (KV n r c d c'))
     -- ^ Given an identifier, lookup in the cache the value
-    , putPersistant :: (Monad m) => Context -> KV n r c d c' ->  m (KV n r c d c', Context)
+    , putPersistant :: (Monad m) => KV n r c d c' -> StateT Context m (KV n r c d c')
     -- ^ Put this content into the persistant medium
-    , putCached     :: (Monad m) => Context -> KV n r c d c' -> m ()
+    , putCached     :: (Monad m) => KV n r c d c' -> StateT Context m ()
     -- ^ Put this content into the cache medium
-    , delPersistant :: (Monad m) => Context -> KVIdentifier n -> m Bool
+    , delPersistant :: (Monad m) => KVIdentifier n -> StateT Context m Bool
     -- ^ Permanently remove (not 'invalidate') the entity
     -- from the persistant medium
-    , delCached     :: (Monad m) => Context -> KVIdentifier n -> m Bool
+    , delCached     :: (Monad m) => KVIdentifier n -> StateT Context m Bool
     -- ^ Clear this entry from the cache (not 'invalidate')
     }
 
 instance Default (Storage n r c d c') where
     def = Storage
-        { getPersistant = \_ _ -> return Nothing
-        , getCached = \_ _ -> return Nothing
-        , putPersistant = \a b -> return (b, a)
-        , putCached = \_ _ -> return ()
-        , delPersistant = \_ _ -> return False
-        , delCached = \_ _ -> return False
+        { getPersistant = \_ -> return Nothing
+        , getCached = \_ -> return Nothing
+        , putPersistant = \a -> return a
+        , putCached = \_ -> return ()
+        , delPersistant = \_ -> return False
+        , delCached = \_ -> return False
         }
 
-put :: (Monad m) => Context -> Storage n r c d c' -> KV n r c d c' -> m ()
-put c' store kv = do
-    M.when (Map.size c' == 0) $ fail failMessage
-    (kv', c) <- putPersistant store c' kv
+putS :: (Monad m) => Storage n r c d c' -> KV n r c d c' -> StateT Context m ()
+putS store kv = do
+    kv' <- putPersistant store kv
     -- ↑ other storages may modify the kv, such as
     -- riak, since it may resolve merge conflicts. 
-    putCached store (Map.union c c') kv'
-{-# INLINABLE put #-}
+    putCached store kv'
+{-# INLINABLE putS #-}
 
-get :: (Monad m) => Context -> Storage n r c d c' -> KVIdentifier n -> m (Maybe (KV n r c d c', Context))
-get c' store i = do
-    M.when (Map.size c' == 0) $ fail failMessage
-    ckv <- getCached store c' i
+getS :: (Monad m) => Storage n r c d c' -> KVIdentifier n -> StateT Context m (Maybe (KV n r c d c'))
+getS store i = do
+    ckv <- getCached store i
     case ckv of
-        Just (kv, _) -> return ckv
+        Just kv -> return ckv
         Nothing -> do
-            pkv <- getPersistant store c' i
-            F.forM_ pkv (\(v, c) -> putCached store (Map.union c c') v)
+            pkv <- getPersistant store i
+            F.forM_ pkv (\v -> putCached store v)
             return pkv
-{-# INLINABLE get #-}
+{-# INLINABLE getS #-}
 
-del ::  (Monad m) => Context -> Storage n r c d c' -> KVIdentifier n -> m Bool
-del c store i =
-    M.liftM or $ sequence [c', p]
+delS ::  (Monad m) => Storage n r c d c' -> KVIdentifier n -> StateT Context m Bool
+delS store i =
+    M.liftM or $ sequence [c, p]
     where
-        p = delPersistant store c i
-        c' = delCached store c i
-{-# INLINABLE del #-}
+        p = delPersistant store i
+        c = delCached store i
+{-# INLINABLE delS #-}
 
-invalidate :: (Monad m) => Context -> Storage n r c d c' -> KVIdentifier n -> m ()
-invalidate c' store i =
-    get c' store i >>= F.mapM_ (\(v, c) -> put (Map.union c c') store (K.invalidate v))
+-- | Updates the record in the database to be invalid
+-- Must be of the right type to deserialize and reserialize properly
+invalidate :: (Monad m) => Storage n r c d c' -> KVIdentifier n -> StateT Context m ()
+invalidate store i =
+    getS store i >>= F.mapM_ (\v -> putS store (K.invalidate v))
 {-# INLINABLE invalidate #-}
 
+-- | Empty context constant
 noContext :: Context
 noContext = Map.empty
 {-# INLINABLE noContext #-}
 
+-- | Adds the type into the context map.
+--
+-- Note: Types are their own record and inserting the
+-- same type twice will not preserve the old value of
+-- that type.
 addToContext :: (T.Typeable t) => Context -> t -> Context
 addToContext c t = Map.insert (T.typeOf t) (D.toDyn t) c
 {-# INLINABLE addToContext #-}
 
 infixl 7 ~+=:
 
+-- | infix version of 'addToContext'
 (~+=:) :: (T.Typeable t) => Context -> t -> Context
 c ~+=: t = addToContext c t
 {-# INLINABLE (~+=:) #-}
-
-failMessage = "Empty Context Provided, no possible outcome!"
-{-# INLINABLE failMessage #-}
 
 contextWrap' :: (T.Typeable k) => k -> Context -> (k -> a) -> a -> a
 contextWrap' k c f df =
@@ -121,6 +126,8 @@ contextWrap' k c f df =
                 Just p -> f p
     where
         key = T.typeOf k
+-- | Helps unwrap the context value desired.
+-- If the value is not present, the default action will be executed.
 contextWrap :: (T.Typeable k)
             => Context -- ^ Context anticipated to have feature
             -> (k -> a)
