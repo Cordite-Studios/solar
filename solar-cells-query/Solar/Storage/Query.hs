0f85c68b9db851363860003c5a82f3fedefbe027
Levi Schuck <levi.schuck@gmail.com>
2013-10-05 10:37:03 -0600

    Updating to the current API

--- solar-cells-query/Solar/Storage/Query.hs
+++ solar-cells-query/Solar/Storage/Query.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE ImplicitParams #-}
 {- |
 A collection of 'Pipe'-based functions which make querying for
 workers easier.
@@ -13,32 +12,33 @@ module Solar.Storage.Query
 where
 
 import           Pipes
-import           Control.Monad.Trans.State as ST
+import           Control.Monad.Trans.RWS as R
 import           Solar.Data.KV
-import           Solar.Storage.Types
-import           Solar.Storage.Functions
+import           Solar.Storage
+import           Solar.Storage.Context
+import           Data.Monoid
 import           Control.Monad(forM_, forever, when)
 
 -- | Given filtering functions, we can use take in a 'KV',
 -- iterate over the links and produce 'KV's which satisfy and
 -- type check
-joinRelation ::  (StorageF n r c d c' m a, StorageF n r c d' c'' m a, ?s :: a, Monad m)
+joinRelation ::  (StorageFC n r c d c' m a, StorageFC n r c d' c'' m a, Monad m, Monoid b)
              => (KVLink n r c -> Bool) -- ^ Link filter
              -> (KV n r c d' c'' -> Bool) -- ^ Resolved entity filter
-             -> Pipe (KV n r c d c') (KV n r c d' c'') (StateT Context m) ()
+             -> Pipe (KV n r c d c') (KV n r c d' c'') (RWST a b Context m) ()
 joinRelation f f' = getRelations f >-> getTaggedKV >-> filterKV f'
 
 -- | Filters incoming 'KV's according to a boolean function
-filterKV :: (StorageF n r c d c' m a, ?s :: a, Monad m)
+filterKV :: (StorageFC n r c d c' m a, Monad m, Monoid b)
          => (KV n r c d c' -> Bool) -- ^ Function to filter with
-         -> Pipe (KV n r c d c') (KV n r c d c') (StateT Context m) ()
+         -> Pipe (KV n r c d c') (KV n r c d c') (RWST a b Context m) ()
 filterKV f = forever $ do
     kv <- await
     when (f kv) $ yield kv
 
 -- | Using a 'TaggedIdentifier', this will produce 'KV's that typecheck
-getTaggedKV ::  (StorageF n r c d c' m a, ?s :: a, Monad m)
-            => Pipe (TaggedIdentifier n r c (d n r c) (c' n r c)) (KV n r c d c') (StateT Context m) ()
+getTaggedKV ::  (StorageFC n r c d c' m a, Monad m, Monoid b)
+            => Pipe (TaggedIdentifier n r c (d n r c) (c' n r c)) (KV n r c d c') (RWST a b Context m) ()
 getTaggedKV = forever $ do
     i' <- await
     let i = untagIdentifier i'
@@ -49,9 +49,9 @@ getTaggedKV = forever $ do
 
 -- | Given a 'KV', it will produce 'TaggedIdentifier's for the
 -- entities that are of interest.
-getRelations ::  (StorageF n r c d c' m a, ?s :: a, Monad m)
+getRelations ::  (StorageFC n r c d c' m a, Monad m, Monoid b)
               => (KVLink n r c -> Bool) -- ^ Link Filter
-              -> Pipe (KV n r c d c') (TaggedIdentifier n r c d' c'') (StateT Context m) ()
+              -> Pipe (KV n r c d c') (TaggedIdentifier n r c d' c'') (RWST a b Context m) ()
 getRelations f = forever $ do
     kv <- await
     let links = relations.meta $ kv
